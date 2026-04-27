278bd75d7d56320647384132704c5dfd3c1b20d9
boerge42 <boerge42@users.noreply.github.com>
2026-04-27 19:57:08 +0200

    ESP-IDF System-API (Systemzeit) statt ezTime
    * ESP-IDF System-API (Systemzeit) statt ezTime verwenden (incl. NTP-Synchronisation)
    * loop() umstrukturiert, 
    * "komische" Berechnungen zu "Qualität der Uhrzeit" und deren Anzeige auf Nixies (blinkende Punkte)
    * diverse berechnete Werte/Zustände via MQTT in die "Welt" senden

--- Version2/esp_nixie_v2.ino
+++ Version2/esp_nixie_v2.ino
@@ -26,16 +26,23 @@
 * Funktionen:
 * -----------
 * - Anmeldung im WLAN                    
-*   - Sanduhr bei WLAN-Connect auf Nixies
-*   - automatischer Reconnect, wenn WLAN weg
-*   - Anzeige, wenn keine Verbindung zum WLAN
-* - mit "Internetzeit" synchronisieren   
+*   - "Sanduhr" bei WLAN-Connect auf Nixies
+*   - automatischer Reconnect, wenn WLAN/MQTT weg
+* - mit "Internetzeit" (NTP) synchronisieren   
 * - Helligkeitssensor (BH1750) eingebunden, um die Nixies dunkel
 *   zu schalten, wenn es dunkel ist
-* - regelmaessig alle Nixies "scrollen"      
+* - regelmaessig (...alle 5min...) alle Nixies "scrollen"      
 * - Ausgabe Uhrzeit (hh.mm.ss) auf Nixies :-)
-* - Steuerung mit MQTT-Nachrichten
-* 
+* - Steuerung mit MQTT-Nachrichten -> siehe weiter unten!
+* - Anzeige Status (binkend; Nummerierung von recht=P0 nach links=P5):
+*   -> P0 --> wenn keine Verbindung zum WLAN
+*   -> P1 --> wenn time_quality < 1
+*   -> P3 --> wenn time_quality < 2
+*   -> P5 --> wenn time_quality < 3
+* - diverse Werte und Status werden zu einem MQTT-Broker gesendet
+
+
+
 *
 * Steuerung durch MQTT-Nachrichten (Topic siehe Quelltext):
 * ---------------------------------------------------------
@@ -66,41 +73,44 @@
 * Have fun!
 *
 ************************************************************************
+inspiriert von:
+  https://www.werner.rothschopf.net/201802_arduino_esp8266_ntp.htm
+  https://randomnerdtutorials.com/esp32-date-time-ntp-client-server-arduino/
+  https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/system_time.html
 */
 
 #include <ESP8266WiFi.h>
-#include <ezTime.h>
 #include <Wire.h>
 #include <BH1750FVI.h>
 #include <PubSubClient.h>
+#include <time.h>
 
+// time
+time_t now;                         // Unixzeit
+tm tm;                              // Structure tm enthaelt die Zeitinformationen
+
+/* Configuration of NTP */
+#define MY_NTP_SERVER "10.1.1.1"           
+#define MY_TZ "CET-1CEST,M3.5.0/02,M10.5.0/03" // https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
 
 // WiFi
-const char *ssid     = "xxxx";
-const char *password = "yyyy";
+const char *ssid     = "xxxxx";
+const char *password = "yyyyy";
+String hostname      = "Nixie";
 
 // MQTT
-//~ #define MQTT_CLIENT_ID "nixie"
 char mqttServer[30] 	= "nanotuxedo";
 int  mqttPort 		    = 1883;
 char mqttUser[30] 	    = "";
 char mqttPassword[30]   = "";
 char mqttClientId[30]   = "nixie";
 const char *mqtt_topic  = "nixie/";
+const char *mqtt_topic_quality  = "nixie/quality/";
 
 WiFiClient espClient;
 PubSubClient mqtt_client(espClient);
 
-// ezTime & Zeitzone
-// ...wenn die beiden folgenden Defines gesetzt sind, werden in setup() 
-// jeweils auch die entsprechenden ezTime-Routinen zum Setzen aufgerufen 
-#define EZTIME_NTP_SERVER      "10.1.1.1"
-#define EZTIME_NTP_INTERVAL    3601  //Sekunden; wenn 0, dann kein NTP-Update!
-
-#define MY_TIME_ZONE           "Europe/Berlin"
-Timezone myTZ;
-char my_time_zone[50] = MY_TIME_ZONE;
-
+boolean wifi_connect = false;
 
 // Helligkeitssensor
 BH1750FVI lux(0x23);
@@ -135,7 +145,7 @@ int display_mode = DISPLAY_TIME;
 
 // ein paar Parameter
 #define NIXIE_SCROLL_EVERY_MINUTE   5   // Minute%... == 0
-#define NIXIE_OFF_LUX               5   // BH1750-Lux-Wert
+#define NIXIE_OFF_LUX               1   // BH1750-Lux-Wert
 
 #define SWITCH_BACK_TO_TIME         5   // Sekunden
 int switch_back_to_time_counter = 0; 
@@ -168,6 +178,32 @@ int switch_back_to_time_counter = 0;
 #define SCL     5   // GPIO5 -> D1
 
 
+// ein paar Zustandvariablen
+uint8_t isNTPSync       = 0;
+uint8_t isWLANStatus    = 0;
+uint8_t isMQTTStatus    = 0;
+uint8_t isNixieScroll   = 0;
+float   isLuxValue      = 0;
+int     time_quality    = 0;   // 0 = keine Zeit, 1 = schlecht, 2 = ok, 3 = gut
+
+#define JITTER_MAX_Q3   50
+#define JITTER_MAX_Q2   200
+
+
+// ********************************************************************************
+// erstes Holen ntp-Zeit in...
+uint32_t sntp_startup_delay_MS_rfc_not_less_than_60000 ()
+{
+  return 1000; // 1s
+}
+
+// ********************************************************************************
+// ntp-Abfrageintervall
+uint32_t sntp_update_delay_MS_rfc_not_less_than_15000 ()
+{
+  return 1 * 60 * 60 * 1000UL; // 1h
+}
+
 // ********************************************************************************
 void data2nixie()
 {
@@ -193,6 +229,7 @@ void nixie_scroll()
 {
     int scroll_counter;
     Serial.println("==> nixie_scroll()");
+    isNixieScroll = 1;
     //
     sr_data.sr_array.d[0] = 0b00111111;
     for (scroll_counter=0; scroll_counter < 10; scroll_counter++) {
@@ -211,7 +248,7 @@ void nixie_scroll()
 void nixie_off()
 {
     Serial.print("==> nixie_off(): ");
-    Serial.println(lux.getLux());
+    Serial.println(isLuxValue);
     // 
     sr_data.sr_array.d[0] = 0b00000000;
     sr_data.sr_array.d[1] = 0b11111111;
@@ -223,34 +260,55 @@ void nixie_off()
 // ********************************************************************************
 void nixie_display_date_time()
 {
+    static boolean point_on = true;
+    
     int d01, d23, d45;
     if (display_mode == DISPLAY_TIME) {
         // Uhrzeit
-        d01 = myTZ.second();
-        d23 = myTZ.minute();
-        d45 = myTZ.hour();
+        d01 = tm.tm_sec;
+        d23 = tm.tm_min;
+        d45 = tm.tm_hour;
     } else if (display_mode == DISPLAY_DATE) {
         // Datum
-        d01 = myTZ.year() - 2000;
-        d23 = myTZ.month();
-        d45 = myTZ.day();        
+        d01 = tm.tm_year - 100;     // Anz. Jahre seit 1900
+        d23 = tm.tm_mon+1;          // Januar = 0
+        d45 = tm.tm_mday;        
     }
-    char str[50];
-    sprintf(str, "==> nixie_display_date_time(): %02d.%02d.%02d", d45, d23, d01);
-    Serial.println(str);
-    //
-    sr_data.sr.p5 = 0;
-    sr_data.sr.p4 = 1;
-    sr_data.sr.p3 = 0;
-    sr_data.sr.p2 = 1;
-    sr_data.sr.p1 = 0;
-    // wenn keine Verbindung zum WLAN oder ein "geplanter" NTP-Sync ausgeblieben ist,
-    // dann letzten Punkt leuchten lassen
-    if ((WiFi.status() == WL_CONNECTED) && (timeStatus() != timeNeedsSync)) {
-        sr_data.sr.p0 = 0;
+    
+    // wenn time_quality < 3 -> blinken
+    if ((time_quality < 3) && point_on) {
+        sr_data.sr.p5 = 1;
     } else {
-        sr_data.sr.p0 = 1;
+        sr_data.sr.p5 = 0;
     }
+    
+    // bei date/time immer an
+    sr_data.sr.p4 = 1;
+    
+    // wenn time_quality < 2 -> blinken
+    if ((time_quality < 2) && point_on) {
+        sr_data.sr.p3 = 1;
+    } else {
+        sr_data.sr.p3 = 0;
+    }
+    
+    // bei date/time immer an
+    sr_data.sr.p2 = 1;
+    
+    // wenn time_quality < 1 -> blinken
+    if ((time_quality < 1) && point_on) {
+        sr_data.sr.p1 = 1;
+    } else {
+        sr_data.sr.p1 = 0;
+    }
+    
+    // wenn keine Verbindung zum WLAN ==>> blinken
+    if (!wifi_connect && point_on) {
+        sr_data.sr.p0 = 1;
+    } else {
+        sr_data.sr.p0 = 0;
+    }
+    
     sr_data.sr.d0 = d01/10;
     sr_data.sr.d1 = d01%10;
     sr_data.sr.d2 = d23/10;
@@ -258,6 +316,14 @@ void nixie_display_date_time()
     sr_data.sr.d4 = d45/10;
     sr_data.sr.d5 = d45%10;
     data2nixie();
+    
+    if (!wifi_connect || (time_quality < 3)) {
+        if (point_on) {
+            point_on = false;
+        } else {
+            point_on = true;
+        }
+    }
 }
 
 // ********************************************************************************
@@ -334,13 +400,12 @@ void nixie_display_mqtt_payload(byte* payload, unsigned int length)
     data2nixie();
 }
 
-
 // ********************************************************************************
 void nixie_display_busy()
 {
     static int counter = 0;
     static int add = 1;
-    Serial.println("==> nixie_display_busy() ");
+    Serial.println("==>> nixie_display_busy() ");
     //
     sr_data.sr_array.d[0] = 0b00000000;
     sr_data.sr_array.d[1] = 0b11111111;
@@ -357,13 +422,13 @@ void nixie_display_busy()
 // **************************************************************
 void mqtt_reconnect ()
 {
-    // ...Verbindungsvesuch
+    // ...Verbindungsversuch
     if (mqtt_client.connect(mqttClientId)) {
-       Serial.println("MQTT connected!"); 
+       Serial.println("==>> mqtt_reconnect(): MQTT connected!"); 
        mqtt_client.publish(mqtt_topic,"hello from nixie!");
        mqtt_client.subscribe(mqtt_topic); 
     } else {
-        Serial.print("MQTT-Connect failed with state ");
+        Serial.print("==>> mqtt_reconnect(): MQTT-Connect failed with state ");
         Serial.println(mqtt_client.state());
     }
 }
@@ -371,7 +436,6 @@ void mqtt_reconnect ()
 // **************************************************************
 void mqtt_callback (char* topic, byte* payload, unsigned int length)
 {
-
     // Payload in String umwandeln 
     String message; 
     for (int i = 0; i < length; i++) { 
@@ -379,7 +443,7 @@ void mqtt_callback (char* topic, byte* payload, unsigned int length)
         }
 
     // Ausgabe topic/payload
-    Serial.print("MQTT-Message arrived: topic=");
+    Serial.print("==>> mqtt_callback(): MQTT-Message arrived: topic=");
     Serial.print(topic);
     Serial.print("; payload=");
     Serial.print(message);
@@ -403,11 +467,108 @@ void mqtt_callback (char* topic, byte* payload, unsigned int length)
 }
 
 // ********************************************************************************
+// ...ueber sie Sinnhaftigkeit der hier berechneten Werte lässt sich streiten
+void update_ntp_quality()
+{
+    struct timeval tv;
+    gettimeofday(&tv, nullptr);
+    
+    time_t now = tv.tv_sec;
+    
+    static unsigned long display_counter = 0;
+    
+    static boolean ntp_initialized  = false;
+    static time_t  ntp_lastGoodSync = 0;
+    static struct  timeval ntp_lastTv = {0, 0};
+    static float  ntp_jitter       = 0;
+
+    // Ist bereits eine realistische Uhrzeit gesetzt? Wenn nein, ist
+    // alles egal und raus! 
+    if (now < 1700000000) {  
+        ntp_initialized = false;
+        time_quality = 0;
+        return;
+    }
+
+    // Es ist eine realistische Uhrzeit gesetzt, also hat der 1.NTP-Sync. 
+    // funktioniert. Aber erst im nächsten Durchlauf kann man korrekte
+    // Qualitätswerte (1s-Diff, Jitter) berechnen...
+    if (!ntp_initialized) {
+        isNTPSync = 1;
+        ntp_initialized = true;
+        ntp_lastGoodSync = now;
+        ntp_lastTv = tv;
+        time_quality = 3;
+        return;
+    }
+
+    // vergangene Zeit (in Mikrosekunden) seit letzten Aufruf (...1000ms) 
+    long diff = (tv.tv_sec - ntp_lastTv.tv_sec) * 1e6 + (tv.tv_usec - ntp_lastTv.tv_usec); // ...in Mikrosekunden!
+    
+    // Jitter berechnen (Abweichung vom Ideal 1s; mit Glaettung; exponentieller gleitender Mittelwert)
+    ntp_jitter = 0.9 * ntp_jitter + 0.1 * fabs(diff - 1000000); // in Mikrosekunden!
+    
+    // Wenn die Zeit um mehr als 20000us "springt", könnte es ein erfolgreicher
+    // NTP-Request gewesen sein
+    if (fabs(diff - 1000000) > 20000) {
+        isNTPSync = 1;
+        ntp_lastGoodSync = now;
+    }
+    
+    ntp_lastTv = tv;    
+    
+    // Zeit-Qualität aus Alter des letzten NTP-Sync. und Jitter ableiten
+    // Wobei die Frage zugelassen ist, warum Jitter ein Kriterium sein soll 
+    // --> keine Ahnung, war nur so ein Bauchgefühl :-)
+    unsigned long age = now - ntp_lastGoodSync;
+
+    if (age < 3600 && ntp_jitter < JITTER_MAX_Q3) {          // letzte ntp-sync älter als 1h und jitter (in Mikrosekunden)
+        time_quality = 3; // sehr gut
+    }
+    else if (age < 21600 && ntp_jitter < JITTER_MAX_Q2) {    // letzte ntp-sync älter als 6h und jitter (in Mikrosekunden)
+        time_quality = 2; // gut
+    }
+    else if (age < 86400) {                                  // letzte ntp-sync älter als 24h
+        time_quality = 1; // ok
+    }
+    else {
+        time_quality = 0; // schlecht
+    }
+    
+    // diverse Werte/Zustände via MQTT senden
+    char mqtt_payload[300];
+    sprintf(mqtt_payload, 
+            "nixie jitter=%f,jitterMaxQ3=%d,jitterMaxQ2=%d,diff_1s=%d,quality=%d,isNTPSync=%d,isWLANStatus=%d,isMQTTStatus=%d,isNixieScroll=%d,isLuxValue=%f", 
+            ntp_jitter,
+            JITTER_MAX_Q3,
+            JITTER_MAX_Q2, 
+            (diff-1000000), 
+            time_quality,
+            isNTPSync,
+            isWLANStatus,
+            isMQTTStatus,
+            isNixieScroll,
+            isLuxValue);
+    //Serial.println(mqtt_payload);
+    mqtt_client.publish(mqtt_topic_quality,mqtt_payload);
+    
+    // ...gesendete Zustände wieder zurücksetzen
+    isNTPSync       = 0;
+    isWLANStatus    = 0;
+    isMQTTStatus    = 0;
+    isNixieScroll   = 0;
+    
+}
+
+// ********************************************************************************
 void setup(void)
 {
+    
+    // ESP auf CPU-Freq. 160MHz stellen, vielleicht nützt es was ;-)
+    system_update_cpu_freq(160);
+    
     // serielle Schnittstelle initialisieren
     Serial.begin(115200);
-    Serial.println("");
     
     // GPIOs
     pinMode(SR_SER, OUTPUT);
@@ -422,25 +583,31 @@ void setup(void)
 
     // Verbindung mit WLAN herstellen
     Serial.println("connecting to wifi:");
+    WiFi.hostname(hostname.c_str());
+    WiFi.persistent(false);
+    WiFi.setAutoReconnect(true);
+    
+    // ...WiFi-Events registrieren
+    WiFi.onStationModeGotIP([](const WiFiEventStationModeGotIP& evt) {
+        wifi_connect = true;
+        Serial.println("WiFi connected");
+    });
+    WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected& evt) {
+        wifi_connect = false;
+        Serial.println("WiFi lost");
+    });
+    // ...
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
       delay(250);
       Serial.print(".");
       nixie_display_busy();
     }
+    wifi_connect = true;
     Serial.println("");
     
-    // Datum/Uhrzeit holen
-    #ifdef EZTIME_NTP_SERVER
-        setInterval(EZTIME_NTP_INTERVAL);
-    #endif
-    #ifdef EZTIME_NTP_INTERVAL
-        setServer(EZTIME_NTP_SERVER);
-    #endif
-    waitForSync();
-    Serial.println("UTC: " + UTC.dateTime());
-    myTZ.setLocation(my_time_zone);
-    Serial.println("MyTZ time: " + myTZ.dateTime("d-M-y H:i:s"));    
+    // NTP initialisieren/ Uhrzeit sync
+    configTime(MY_TZ, MY_NTP_SERVER);
     
     // MQTT initialisieren/verbinden
     mqtt_client.setServer(mqttServer, mqttPort);
@@ -457,33 +624,43 @@ void loop()
 
     // ...vorhergehende Sekunde
     static uint8_t old_second = -1;
+    
+    // ...wifi_connect_counter
+    static unsigned long wifi_t = 0;
 
-    // ...jede Sekunde schauen, was zu tun ist...
-    if (myTZ.second() != old_second) {
-        old_second = myTZ.second();
+    // ...mqtt_connect_counter
+    static unsigned long mqtt_t = 0;
+
+    // ...lux_counter und Schalter
+    static unsigned long lux_t = 0;
+    static boolean is_dark = false;
+
+    // ...ntp_quality_counter
+    static unsigned long ntp_quality_t = 0;
+
+    time(&now);                       // aktuelle Zeit...
+    localtime_r(&now, &tm);           // ...in lokale Zeit
+    
+    // jede Sekunde Zeit-Qualitaet bestimmen
+    if(millis() - ntp_quality_t >= 1000) {
+        ntp_quality_t = millis();
+        update_ntp_quality();
+    }
+
+    // ...jede Sekunde Anzeige entsprechend aktualisieren
+    if (tm.tm_sec != old_second) {
+        old_second = tm.tm_sec;
                 
-       // wenn keine Verbindung zum WLAN besteht, jede (volle) Minute einen Reconnect versuchen
-        if ((WiFi.status() != WL_CONNECTED) && (old_second%60 == 0)) {
-           Serial.println("reconnecting to wifi...");
-           WiFi.disconnect();
-           WiFi.reconnect();
-        }                
-        
-        // mit MQTT alles in Ordnung?
-        if (!mqtt_client.connected()) {
-            mqtt_reconnect();
-        }
-                
-        // sollen Nixies ueberhaupt etwas anzeigen (Umgebungshelligkeit ausreichend bzw. via MQTT ausgeschaltet)
-        if ((lux.getLux() < NIXIE_OFF_LUX) || (display_mode == DISPLAY_OFF)) {
+        // sollen Nixies überhaupt etwas anzeigen (Umgebungshelligkeit ausreichend bzw. via MQTT ausgeschaltet)
+        if (is_dark || (display_mode == DISPLAY_OFF)) {
             // ...nein...
             nixie_off();
             // ...Flanke "Lux_off" -> "Lux_on": display_mode = DISPLAY_TIME...???
         } else {
             // ...ja...
             // Nixies zu jeder "definierten" Minute scrollen!?
-            if ((myTZ.second() == 0) && 
-                (myTZ.minute() % NIXIE_SCROLL_EVERY_MINUTE == 0) && 
+            if ((tm.tm_sec == 0) && 
+                (tm.tm_min % NIXIE_SCROLL_EVERY_MINUTE == 0) && 
                 ((display_mode == DISPLAY_DATE) || (display_mode == DISPLAY_TIME))) {
                 nixie_scroll();
             }
@@ -502,13 +679,42 @@ void loop()
         
     }
 
+    // jede 5min prüfen, ob WLAN-Verbindung besteht
+    if (millis() - wifi_t > 300000) {
+        wifi_t = millis();
+        isWLANStatus = 1;
+        if (WiFi.status() == WL_CONNECTED) {
+            Serial.println("==> loop(): WiFi connected.");
+            wifi_connect = true; 
+        } else {
+            wifi_connect = false; 
+            Serial.println("==>> loop(): WiFi not connected!");
+            WiFi.reconnect();
+        }
+    }
+    
+    // jede 5min prüfen, ob mit MQTT alles in Ordnung?
+    if (millis() - mqtt_t > 300000) {
+        mqtt_t = millis();
+        isMQTTStatus = 1;
+        if (!mqtt_client.connected()) {
+            Serial.println("==>> loop(): MQTT not connected!");
+            mqtt_reconnect();
+        }
+    }
+
+    // jede 1s lux-Sensor auslesen und entspr. Boolean setzen
+    if (millis() - lux_t > 1000) {
+        lux_t = millis();
+        isLuxValue = lux.getLux();
+        if (isLuxValue < NIXIE_OFF_LUX) {
+            is_dark = true;
+        } else {
+            is_dark = false;
+        }
+    }
+
     // MQTT-loop
     mqtt_client.loop();
-
-    // ezTime-loop
-    events();    
-
-    // Verschnaufspause ;-)
-    delay(5);
 
 }
